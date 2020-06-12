d0bba80afc91624285383004fd7e12ab45d02f77
shamilkhan <shamilkhan1999@gmail.com>
2020-06-12 18:36:51 +0300

    replace date to timestamp

--- src/pages/time-data/time-component/index.tsx
+++ src/pages/time-data/time-component/index.tsx
@@ -8,7 +8,7 @@ const TimeComponent = ({ timestamp }: Props) => {
   return (
     <div className="time-data__value">
       <div className="time-data__icon">🕔</div>
-      <div className="time-data__timestamp">{`${new Date(timestamp).toLocaleString()}`}</div>
+      <div className="time-data__timestamp">{`${timestamp}`}</div>
     </div>
   );
 };
