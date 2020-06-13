0f804c954043a50bae48d103f22807237e250ff7
shamilkhan <shamilkhan1999@gmail.com>
2020-06-13 06:04:02 +0300

    add loadable components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 //@ts-nocheck
-import React, { Suspense } from "react";
+import React from "react";
+import loadable from "@loadable/component";
 import useToken from "./custom-hooks/use-token";
 import "./App.css";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
-const LoginPage = React.lazy(() => import("./pages/login"));
-const TimeDataPage = React.lazy(() => import("./pages/time-data"));
+const LoginPage = loadable(() => import("./pages/login"));
+const TimeDataPage = loadable(() => import("./pages/time-data"));
 
 function App() {
   const token = useToken();
-
-  return (
-    <div className="App">
-      {token ? (
-        <Suspense fallback={() => <div>loading</div>}>
-          <TimeDataPage />
-        </Suspense>
-      ) : (
-        <Suspense fallback={() => <div>loading</div>}>
-          <LoginPage />
-        </Suspense>
-      )}
-    </div>
-  );
+  return <div className="App">{token ? <TimeDataPage /> : <LoginPage />}</div>;
 }
 
 export default App;
