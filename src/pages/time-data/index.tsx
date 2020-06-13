5464a469e15ac4daf6227995f571c4cdb5d00576
shamilkhan <shamilkhan1999@gmail.com>
2020-06-13 05:46:56 +0300

    add some refactoring to connection logic

--- src/pages/time-data/index.tsx
+++ src/pages/time-data/index.tsx
@@ -9,7 +9,6 @@ import "./index.scss";
 const TimeData = () => {
   const token = useToken();
   //@ts-ignore
-  let socket: WebSocket = null;
   let connectionAb = new AbortController();
   const [wsUrl, setWsUrl] = useState("");
   const [serverDate, setServerDate] = useState<number>(0);
@@ -17,27 +16,28 @@ const TimeData = () => {
 
   /**@description Get connection string  Effect */
   useEffect(() => {
-    getConnectionUrl();
+    if (!isConnected) getConnectionUrl();
   }, [isConnected]);
 
   /**@description Connect to WS server */
   useEffect(() => {
-    connect();
-    return () => {
-      if (socket) {
-        socket.close();
-      }
+    if (!wsUrl) return;
+    const socket = new WebSocket(wsUrl);
+    socket.onopen = (e) => {
+      setIsConnected(true);
+      socket.onmessage = (e: MessageEvent) => {
+        setServerDate(JSON.parse(e.data)["server_time"]);
+      };
+      socket.onclose = () => {
+        setIsConnected(false);
+      };
     };
+    return () => socket.close();
   }, [wsUrl]);
 
   /**@description Clear all side-effects on unmount */
   useEffect(() => {
-    return () => {
-      if (socket) {
-        socket.close();
-      }
-      connectionAb.abort();
-    };
+    return () => connectionAb.abort();
   }, []);
 
   const getConnectionUrl = useCallback(() => {
@@ -53,24 +53,6 @@ const TimeData = () => {
       })
       .catch((err) => console.log("subscribe error is", err));
   }, []);
-
-  const connect = () => {
-    if (!wsUrl) return;
-    if (socket) {
-      socket.close();
-    }
-    socket = new WebSocket(wsUrl);
-    socket.onopen = (e) => {
-      console.log("Socket open connection");
-      setIsConnected(true);
-      socket.onmessage = (e: MessageEvent) => {
-        setServerDate(JSON.parse(e.data)["server_time"]);
-      };
-      socket.onclose = () => {
-        setIsConnected(false);
-      };
-    };
-  };
 
   return (
     <div className="time-data">
