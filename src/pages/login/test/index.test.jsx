1409d44d22c1d937337a977ec1f6ffd94a7f7ccc
shamilkhan <shamilkhan1999@gmail.com>
2020-06-13 09:59:22 +0300

    update tests

--- src/pages/login/test/index.test.jsx
+++ src/pages/login/test/index.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Wrapper from "../../../testing-utils/store-wrapper";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Login from "../index";
 
 describe("<Login/>", () => {
@@ -10,10 +10,37 @@ describe("<Login/>", () => {
     expect(submitButtom).toBeInTheDocument();
   });
   it("Rendered Inputs", () => {
-    const { getByPlaceholderText  } = render(<Login />, { wrapper: Wrapper });
+    const { getByPlaceholderText } = render(<Login />, { wrapper: Wrapper });
     const usernameNode = getByPlaceholderText("username");
     expect(usernameNode).toBeInTheDocument();
+    expect(usernameNode.type).toBe("text");
     const passwordNode = getByPlaceholderText("password");
+    expect(passwordNode.type).toBe("password");
     expect(passwordNode).toBeInTheDocument();
+  });
+  it("reaction to change Event", () => {
+    const { getByPlaceholderText } = render(<Login />, { wrapper: Wrapper });
+    const usernameNode = getByPlaceholderText("username");
+    expect(usernameNode.value).toBe("");
+    /**@description Fill input */
+    fireEvent.change(usernameNode, {
+      target: { value: "Sam" },
+    });
+    expect(usernameNode.value).toBe("Sam");
+  });
+  it("test submit", () => {
+    const { getByPlaceholderText, getByText } = render(<Login />, { wrapper: Wrapper });
+    const usernameNode = getByPlaceholderText("username");
+    const passwordNode = getByPlaceholderText("password");
+    const submitButtom = getByText("Login");
+    /**@description Fill Username */
+    fireEvent.change(usernameNode, {
+      target: { value: "Sam" },
+    });
+    /**@description Fill Password */
+    fireEvent.change(passwordNode, {
+      target: { value: "12345" },
+    });
+    fireEvent.click(submitButtom);
   });
 });
